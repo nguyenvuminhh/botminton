7340fb39f4fdf9325da78ea506e55a74bc29cd9d
Vu Minh Nguyen <vu.m.nguyen@aalto.fi>
2025-08-03 18:48:39 +0300

    first

---
+++ __main__.py
@@ -0,0 +1,15 @@
+from commands.start import start
+from config import BOT_TOKEN
+from constants import Command
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
+def run():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler(Command.START, start))
+
+    app.start_polling()
+
+if __name__ == "__main__":
+    run()
