70afae0f360f20e8d11ee198a53cf8cbe748fc87
Vu Minh Nguyen <vu.m.nguyen@aalto.fi>
2025-08-04 00:58:36 +0300

    change id from int to str

--- dtos/period_money.py
+++ dtos/period_money.py
@@ -2,7 +2,7 @@ from datetime import date
 from pydantic import BaseModel, model_validator
 
 class PersonalPeriodMoney(BaseModel):
-    person_id: int
+    person_id: str
     telegram_user_name: str
     period_money: float
 
@@ -14,7 +14,7 @@ class PersonalPeriodMoney(BaseModel):
 class PeriodMoneyReport(BaseModel):
     period_start_date: date
     period_end_date: date
-    period_id: int
+    period_id: str
     personal_period_money: list[PersonalPeriodMoney]
     total_period_money: float
 
@@ -31,10 +31,10 @@ if __name__ == "__main__":
     report = PeriodMoneyReport(
         period_start_date=date(2023, 1, 1),
         period_end_date=date(2023, 1, 31),
-        period_id=1,
+        period_id="1",
         personal_period_money=[
-            PersonalPeriodMoney(person_id=1, telegram_user_name="user1", period_money=100.999),
-            PersonalPeriodMoney(person_id=2, telegram_user_name="user2", period_money=200.0),
+            PersonalPeriodMoney(person_id="1", telegram_user_name="user1", period_money=100.999),
+            PersonalPeriodMoney(person_id="2", telegram_user_name="user2", period_money=200.0),
         ],
         total_period_money=300.999
     )
