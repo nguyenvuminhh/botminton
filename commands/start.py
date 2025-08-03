339281232d5d5b462bb30978afa2271dadb94fe6
Vu Minh Nguyen <vu.m.nguyen@aalto.fi>
2025-08-03 18:48:39 +0300

    first

---
+++ commands/start.py
@@ -0,0 +1,5 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hi")
---
+++ constants.py
@@ -0,0 +1,4 @@
+from enum import Enum, StrEnum, auto
+
+class Command(StrEnum):
+    START = "start"
